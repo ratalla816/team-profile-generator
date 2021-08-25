a81fcb464a6035a22281233f11b8e769399313d8
ratalla816 <85460602+ratalla816@users.noreply.github.com>
2021-08-24 19:57:35 -0500

    stuff I need

---
+++ index.js
@@ -0,0 +1,3 @@
+// stuff I will use: npm inquirer, Jest- validate employee values, async functions, try catch, console logs
+// async await, promisify, else if, require (fs), writeFileAsync,  maybe more stuff..
+// stuff I will need: THE SRC FILES!!!!, classes from lib, inquirer questions, team member array, the array index thing (let i = 0; i < array.length; i++)
