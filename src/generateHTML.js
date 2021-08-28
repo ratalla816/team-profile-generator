d1bcda78b7deac5bdabd6ccdaa477c78706e85b2
ratalla816 <85460602+ratalla816@users.noreply.github.com>
2021-08-28 13:03:14 -0500

    added for loop

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -1,5 +1,36 @@
 const generateHTML= (data) => {
 
+    teamArray = []; 
+
+    for (let i = 0; i < data.length; i++) {
+        const employee = data[i];
+        const role = employee.getRole(); 
+
+
+       
+        if (role === 'Engineer') {
+            const engineerCard = generateEngineer(employee);
+
+            teamArray.push(engineerCard);
+        }
+
+       
+        if (role === 'Intern') {
+            const internCard = generateIntern(employee);
+
+            teamArray.push(internCard);
+        }
+
+        
+        if (role === 'Manager') {
+            const managerCard = generateManager(employee);
+
+            teamArray.push(managerCard);
+        }
+        
+    }
+
+  
 return `<!DOCTYPE html>
 <html>
 
@@ -69,24 +100,19 @@ return `<!DOCTYPE html>
                 ${data.office}
               </h4>
               <ul id="list-addManager" class="list-group list-group-flush">
-              </ul>
-            </div>
-          </div>
- 
-
-
-
+             </ul>
+           </div>
+         </div>
+        </main> 
+ </body>
 <script src="https://code.jquery.com/jquery-3.5.1.min.js"
   integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
 <script src="https://unpkg.com/popper.js@1.16.0/dist/umd/popper.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js"
   integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/moment-with-locales.min.js"></script>
-
-</body>
-
-
 </html>`
+
 }
 
-module.exports = {generateHTML}
+module.exports = generateHTML
